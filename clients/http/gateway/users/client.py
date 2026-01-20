e94225bd51189fe2f36349e0698ab1f77f78375a
tatyderb <tatyderb@gmail.com>
2026-01-21 00:46:23 +0300

    7.5.5 Обертки над _api вызовами в users.client

--- clients/http/gateway/users/client.py
+++ clients/http/gateway/users/client.py
@@ -1,9 +1,31 @@
+import time
 from typing import TypedDict
 
 from httpx import Response
 
 from clients.http.client import HTTPClient
 from clients.http.gateway.client import build_gateway_http_client
+
+
+# Добавили описание структуры пользователя
+class UserDict(TypedDict):
+    """
+    Описание структуры пользователя.
+    """
+    id: str
+    email: str
+    lastName: str
+    firstName: str
+    middleName: str
+    phoneNumber: str
+
+
+# Добавили описание структуры ответа получения пользователя
+class GetUserResponseDict(TypedDict):
+    """
+    Описание структуры ответа получения пользователя.
+    """
+    user: UserDict
 
 
 class CreateUserRequestDict(TypedDict):
@@ -15,6 +37,13 @@ class CreateUserRequestDict(TypedDict):
     firstName: str
     middleName: str
     phoneNumber: str
+
+# Добавили описание структуры ответа создания пользователя
+class CreateUserResponseDict(TypedDict):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    user: UserDict
 
 
 class UsersGatewayHTTPClient(HTTPClient):
@@ -31,6 +60,10 @@ class UsersGatewayHTTPClient(HTTPClient):
         """
         return self.get(f"/api/v1/users/{user_id}")
 
+    def get_user(self, user_id: str) -> GetUserResponseDict:
+        response = self.get_user_api(user_id)
+        return response.json()
+
     def create_user_api(self, request: CreateUserRequestDict) -> Response:
         """
         Создание нового пользователя.
@@ -39,6 +72,18 @@ class UsersGatewayHTTPClient(HTTPClient):
         :return: Ответ от сервера (объект httpx.Response).
         """
         return self.post("/api/v1/users", json=request)
+
+    def create_user(self) -> CreateUserResponseDict:
+        request = CreateUserRequestDict(
+            email=f"user.{time.time()}@example.com",
+            lastName="string",
+            firstName="string",
+            middleName="string",
+            phoneNumber="string"
+        )
+        response = self.create_user_api(request)
+        return response.json()
+
 
 
 # Добавляем builder для UsersGatewayHTTPClient
