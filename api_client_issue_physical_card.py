eb0c268cfa11d073721a208debfb0f8608bf3295
tatyderb <tatyderb@gmail.com>
2026-01-21 23:13:02 +0300

    7.5.7 Обертки над _api вызовами в cards.client, accounts.client.

---
+++ api_client_issue_physical_card.py
@@ -0,0 +1,27 @@
+from clients.http.gateway.accounts.client import build_accounts_gateway_http_client
+from clients.http.gateway.cards.client import build_cards_gateway_http_client
+from clients.http.gateway.users.client import build_users_gateway_http_client
+
+users_gateway_client = build_users_gateway_http_client()
+cards_gateway_client = build_cards_gateway_http_client()
+accounts_gateway_client = build_accounts_gateway_http_client()
+
+# создаем пользователя
+create_user_response = users_gateway_client.create_user()
+print('Create user data:', create_user_response)
+
+user_id = create_user_response['user']['id']
+
+# Открываем дебетовый счет
+open_debit_card_account_response = accounts_gateway_client.open_debit_card_account(user_id=user_id)
+print('Open debit card account response:', open_debit_card_account_response)
+
+account_id = open_debit_card_account_response['account']['id']
+
+# Выпускаем физическую карту
+issue_physical_card_response = cards_gateway_client.issue_physical_card(
+    user_id=user_id,
+    account_id=account_id
+)
+print('Issue physical card response:', issue_physical_card_response)
+
