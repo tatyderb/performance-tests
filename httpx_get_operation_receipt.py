5db0315e01dd2dc7280ae76af689acd37878ccb5
tatyderb <tatyderb@gmail.com>
2026-01-23 00:49:38 +0300

    7.2.14 Оформление кода согласно требованиям заказчика.

--- httpx_get_operation_receipt.py
+++ httpx_get_operation_receipt.py
@@ -1,117 +1,41 @@
-"""
-Решение задачи https://stepik.org/lesson/1798961/step/14
-Оформляя каждый ендпоинт в отдельную функцию мы получаем copy-paste (унифицированный) код без лишнего
-переименования переменных.
-Который удобно сворачивать/разворачивать в текстовых редакторах.
-А еще лучше - хранить в отдельных от сценария файлах, сгруппировав по бизнес-функцональности эндпоинтов.
-PS: Этому миру не хватает встроенного логирования.
-"""
-
-import json
 import httpx
 import time
 
-DEFAULT_TIMEOUT = 20    # без этого не работает на моем компьютере
+# Данные для создания пользователя
+create_user_payload = {
+    "email": f"user.{time.time()}@example.com",
+    "lastName": "string",
+    "firstName": "string",
+    "middleName": "string",
+    "phoneNumber": "string"
+}
 
+# Выполняем запрос на создание пользователя
+create_user_response = httpx.post("http://localhost:8003/api/v1/users", json=create_user_payload)
+create_user_response_data = create_user_response.json()
+user_id = create_user_response_data['user']['id']
 
-def create_user() -> str | None:
-    """Создает пользователя с email "user.{timestamp}@example.com"
-    Возвращает userID.
-    """
-    url = "http://localhost:8003/api/v1/users"
-    # Данные для создания пользователя 
-    body = {
-        "email": f"user.{time.time()}@example.com",
-        "lastName": "string",
-        "firstName": "string",
-        "middleName": "string",
-        "phoneNumber": "string"
-    }
+# Создать кредитный счёт для пользователя
+open_credit_card_account_response = httpx.post(
+    "http://localhost:8003/api/v1/accounts/open-credit-card-account",
+    json = {"userId": user_id}
+)
+credit_card_account_json = open_credit_card_account_response.json()
+account_id = credit_card_account_json["account"]["id"]
+card_id = credit_card_account_json["account"]["cards"][0]["id"]
 
-    try:
-        resp = httpx.post(url, json=body)
-        data = resp.json()
+# Покупка
+pushcase_body = {
+  "status": "IN_PROGRESS",
+  "amount": 77.99,
+  "cardId": card_id,
+  "accountId": account_id,
+  "category": "taxi"
+}
+pushcase_response = httpx.post("http://localhost:8003/api/v1/operations/make-purchase-operation", json=pushcase_body)
+operation_id = pushcase_response.json()["operation"]["id"]
 
-        # Выводим полученные данные пользователя
-        # print("Status Code:", resp.status_code)
-        # print("Create user response:", data)
-
-        user_id = data["user"]["id"]
-        return user_id
-    except httpx.HTTPStatusError as e:
-        print(f"Ошибка запроса {url}: {e}")
-    except json.decoder.JSONDecodeError as e:
-        print(f"Ошибка JSON {url}: {e}")
-
-def create_account(user_id: str) -> tuple[str] | None:
-    """Создание кошелька по user_id.
-    Возвращает (account_id, card_id).
-    """
-    url = "http://localhost:8003/api/v1/accounts/open-credit-card-account"
-    body = {
-        "userId": user_id
-    }
-    try:
-        resp = httpx.post(url, json=body, timeout=DEFAULT_TIMEOUT)
-        # print("Status Code:", resp.status_code)
-        
-        data = resp.json()
-        # print("Create account response:", data)
-        
-        return data['account']['id'], data['account']['cards'][0]['id']
-    except httpx.HTTPStatusError as e:
-        print(f"Ошибка запроса {url}: {e}")
-    except json.decoder.JSONDecodeError as e:
-        print(f"Ошибка JSON {url}: {e}")
-        print(resp.text)
-        print(resp.request.content)
-
-def make_purchase_operation(account_id: str, card_id: str) -> str | None:
-    """Покупка. account_id и card_id - заданы, остальные параметры пока захардкожены.
-    Возвращает operation_id.
-    """
-    url = "http://localhost:8003/api/v1/operations/make-purchase-operation"
-    body = {
-          "status": "IN_PROGRESS",
-          "amount": 77.99,
-          "cardId": card_id,
-          "accountId": account_id,
-          "category": "taxi"
-        }
-    try:
-        resp = httpx.post(url, json=body, timeout=DEFAULT_TIMEOUT)
-        # print("Status Code:", resp.status_code)
-        
-        data = resp.json()
-        # print("Purchase response:", data)
-        
-        return resp.json()['operation']['id']
-    except httpx.HTTPStatusError as e:
-        print(f"Ошибка запроса {url}: {e}")
-    except json.decoder.JSONDecodeError as e:
-        print(f"Ошибка JSON {url}: {e}")
-        
-def get_operation_receipt(operation_id: str):
-    """Чек по operation_id.
-    """
-    url = f"http://localhost:8003/api/v1/operations/operation-receipt/{operation_id}"
-    
-    try:
-        resp = httpx.get(url, timeout=DEFAULT_TIMEOUT)
-        # print("Status Code:", resp.status_code)
-        # print("Request:", resp.request.url)
-        
-        data = resp.json()
-        print("Receipt response:", data)
-
-    except httpx.HTTPStatusError as e:
-        print(f"Ошибка запроса {url}: {e}")
-    except json.decoder.JSONDecodeError as e:
-        print(f"Ошибка JSON {url}: {e}")
-
-
-if __name__ == "__main__":
-    user_id = create_user()
-    account_id, card_id = create_account(user_id)
-    operation_id = make_purchase_operation(account_id, card_id)
-    get_operation_receipt(operation_id)
+# чек по операции
+receipt_doc_response = httpx.get(f"http://localhost:8003/api/v1/operations/operation-receipt/{operation_id}")
+receipt_data = receipt_doc_response.json()
+print(receipt_data)
