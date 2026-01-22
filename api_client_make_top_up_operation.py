db74003e604e18d156532326751efda8345df7e7
tatyderb <tatyderb@gmail.com>
2026-01-22 21:39:27 +0300

    7.5.16 Пополнение счета

---
+++ api_client_make_top_up_operation.py
@@ -0,0 +1,25 @@
+from clients.http.gateway.accounts.client import build_accounts_gateway_http_client
+from clients.http.gateway.operations.client import build_operations_gateway_http_client
+from clients.http.gateway.users.client import build_users_gateway_http_client
+
+users_gateway_client = build_users_gateway_http_client()
+accounts_gateway_client = build_accounts_gateway_http_client()
+operations_gateway_client = build_operations_gateway_http_client()
+
+# создаем пользователя
+create_user_response = users_gateway_client.create_user()
+print('Create user response:', create_user_response)
+
+user_id = create_user_response['user']['id']
+
+# Открываем дебетовый счет
+open_debit_card_account_response = accounts_gateway_client.open_credit_card_account(user_id=user_id)
+print('Open debit card account response:', open_debit_card_account_response)
+
+account_id = open_debit_card_account_response['account']['id']
+card_id = open_debit_card_account_response['account']['cards'][0]['id']
+
+# Пополнение счета
+make_debit_operation_response = operations_gateway_client.make_top_up_operation(account_id=account_id, card_id=card_id)
+print('Make top up operation response:', make_debit_operation_response)
+
--- clients/http/gateway/operations/client.py
+++ clients/http/gateway/operations/client.py
@@ -2,9 +2,39 @@ from typing import TypedDict
 from httpx import Response, QueryParams
 
 from clients.http.client import HTTPClient
+from clients.http.gateway.client import build_gateway_http_client
+
+
+class OperationDict(TypedDict):
+    id: str
+    type: str
+    status: str
+    amount: float
+    cardId: str
+    category: str
+    createdAt: str
+    accountId: str
+
+class OperationResponseDict(TypedDict):
+    operation: OperationDict
+
+class OperationReceiptDict(TypedDict):
+    url: str
+    document: str
+
+class OperationsSummaryDict(TypedDict):
+    spentAmount: float
+    receivedAmount: float
+    cashbackAmount: float
+
+class OperationsSummaryResponseDict(TypedDict):
+    summary: OperationsSummaryDict
 
 class GetOperationsQueryDict(TypedDict):
     accountId: str
+
+class GetOperationsResponseDict(TypedDict):
+    operations: list[OperationDict]
 
 class MakeOperationRequestDict(TypedDict):
     status: str
@@ -12,27 +42,47 @@ class MakeOperationRequestDict(TypedDict):
     cardId: str
     accountId: str
 
-class MakeFeeOperationRequestDict(TypedDict):
+class MakeFeeOperationRequestDict(MakeOperationRequestDict):
     pass
 
-class MakeTopUpOperationRequestDict(TypedDict):
+class MakeTopUpOperationRequestDict(MakeOperationRequestDict):
     pass
 
-class MakeCashbackOperationRequestDict(TypedDict):
+class MakeCashbackOperationRequestDict(MakeOperationRequestDict):
     pass
 
-class MakeTransferOperationRequestDict(TypedDict):
+class MakeTransferOperationRequestDict(MakeOperationRequestDict):
     pass
 
-class MakeBillPaymentOperationRequestDict(TypedDict):
+class MakeBillPaymentOperationRequestDict(MakeOperationRequestDict):
     pass
 
-class MakeWithdawalOperationRequestDict(TypedDict):
+class MakeWithdawalOperationRequestDict(MakeOperationRequestDict):
     pass
 
 class MakePurchaseOperationRequestDict(MakeOperationRequestDict):
     category: str
 
+class MakeFeeOperationResponseDict(OperationResponseDict):
+    pass
+
+class MakeTopUpOperationResponseDict(OperationResponseDict):
+    pass
+
+class MakeCashbackOperationResponseDict(OperationResponseDict):
+    pass
+
+class MakeTransferOperationResponseDict(OperationResponseDict):
+    pass
+
+class MakeBillPaymentOperationResponseDict(OperationResponseDict):
+    pass
+
+class MakeWithdawalOperationResponseDict(OperationResponseDict):
+    pass
+
+class MakePurchaseOperationResponseDict(OperationResponseDict):
+    pass
 
 class OperationsGatewayHTTPClient(HTTPClient):
     """
@@ -137,3 +187,167 @@ class OperationsGatewayHTTPClient(HTTPClient):
         :return: Объект httpx.Response с результатом операции.
         """
         return self.post("/api/v1/operations/make-cash-withdrawal-operation", json=request)
+
+    def get_operation(self, operation_id: str) -> OperationResponseDict:
+        """
+        Получение информации по операции по operation_id.
+
+        :param operation_id: Идентификатор операции.
+        :return: Ответ от сервера (объект OperationResponseDict).
+        """
+        response = self.get_operation_api(operation_id=operation_id)
+        return response.json()
+
+    def get_operation_receipt(self, operation_id: str) -> OperationReceiptDict:
+        """
+        Получение чека по операции по operation_id.
+
+        :param operation_id: Идентификатор операции.
+        :return: Ответ от сервера (объект OperationReceiptDict).
+        """
+        response = self.get_operation_receipt_api(operation_id=operation_id)
+        return response.json()
+
+    def get_operations(self, account_id: str) -> GetOperationsResponseDict:
+        """
+        Получение списка операций для определенного счета.
+
+        :param account_id: Id счета.
+        :return: Ответ от сервера (объект GetOperationsResponseDict).
+        """
+        query = GetOperationsQueryDict(accountId=account_id)
+        response = self.get_operations_api(query)
+        return response.json()
+
+    def get_operations_summary(self, account_id: str) -> OperationsSummaryResponseDict:
+        """
+        Получение статистики по операциям для определенного счета.
+
+        :param account_id: Id счета
+        :return: Ответ от сервера (объект OperationsSummaryResponseDict).
+        """
+        query = GetOperationsQueryDict(accountId=account_id)
+        response = self.get_operations_summary_api(query=query)
+        return response.json()
+
+    def make_fee_operation(self, card_id: str, account_id: str) -> MakeFeeOperationResponseDict:
+        request = MakeFeeOperationRequestDict(
+            status="COMPLETED",
+            amount=55.77,
+            cardId=card_id,
+            accountId=account_id
+        )
+        response = self.make_fee_operation_api(request)
+        return response.json()
+
+    def make_top_up_operation(self, card_id: str, account_id: str) -> MakeTopUpOperationResponseDict:
+        """
+        Операции пополнения.
+
+        :param card_id: Id карты
+        :param account_id: Id счета
+        :return: Объект MakeTopUpOperationResponseDict с результатом операции.
+        """
+        body = MakeTopUpOperationRequestDict(
+            status="COMPLETED",
+            amount=55.77,
+            cardId=card_id,
+            accountId=account_id
+        )
+        response = self.make_top_up_operation_api(body)
+        return response.json()
+
+    def make_cashback_operation(self, card_id: str, account_id: str) -> MakeCashbackOperationResponseDict:
+        """
+        Операция кэшбека.
+
+        :param card_id: Id карты
+        :param account_id: Id счета
+        :return: Объект MakeCashbackOperationResponseDict с результатом операции.
+        """
+        body = MakeCashbackOperationRequestDict(
+            status="COMPLETED",
+            amount=55.77,
+            cardId=card_id,
+            accountId=account_id
+        )
+        response = self.make_cashback_operation_api(body)
+        return response.json()
+
+    def make_transfer_operation(self, card_id: str, account_id: str) -> MakeTransferOperationResponseDict:
+        """
+        Операция перевода.
+
+        :param card_id: Id карты
+        :param account_id: Id счета
+        :return: Объект MakeTransferOperationResponseDict с результатом операции.
+        """
+        body = MakeTransferOperationRequestDict(
+            status="COMPLETED",
+            amount=55.77,
+            cardId=card_id,
+            accountId=account_id
+        )
+        response = self.make_cashback_operation_api(body)
+        return response.json()
+
+    def make_purchase_operation(self, card_id: str, account_id: str) -> MakePurchaseOperationResponseDict:
+        """
+        Операция покупки.
+
+        :param card_id: Id карты
+        :param account_id: Id счета
+        :return: Объект MakePurchaseOperationResponseDict с результатом операции.
+        """
+        body = MakePurchaseOperationRequestDict(
+            status="COMPLETED",
+            amount=55.77,
+            cardId=card_id,
+            accountId=account_id,
+            category="taxi"
+        )
+        response = self.make_purchase_operation_api(body)
+        return response.json()
+
+    def make_bill_payment_operation(self, card_id: str, account_id: str) -> MakeBillPaymentOperationResponseDict:
+        """
+        Оплата по счету.
+
+        :param card_id: Id карты
+        :param account_id: Id счета
+        :return: Объект MakeBillPaymentOperationResponseDict с результатом операции.
+        """
+        body = MakeBillPaymentOperationRequestDict(
+            status="COMPLETED",
+            amount=55.77,
+            cardId=card_id,
+            accountId=account_id
+        )
+        response = self.make_bill_payment_operation_api(body)
+        return response.json()
+
+    def make_cash_withdrawal_operation(self, card_id: str, account_id: str) -> MakeWithdawalOperationResponseDict:
+        """
+        Выполняет POST-запрос создания операции снятия наличных.
+
+        :param card_id: Id карты
+        :param account_id: Id счета
+        :return: Объект MakeWithdawalOperationResponseDict с результатом операции.
+        """
+        body = MakeWithdawalOperationRequestDict(
+            status="COMPLETED",
+            amount=55.77,
+            cardId=card_id,
+            accountId=account_id
+        )
+        response = self.make_cash_withdrawal_operation_api(body)
+        return response.json()
+
+
+def build_operations_gateway_http_client() -> OperationsGatewayHTTPClient:
+    """
+    Функция создаёт экземпляр DocumentsGatewayHTTPClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию DocumentsGatewayHTTPClient.
+    """
+    return OperationsGatewayHTTPClient(client=build_gateway_http_client())
