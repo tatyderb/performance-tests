fe4f56ee6bd344f41541ca818f47c6d87f0c6a7f
tatyderb <tatyderb@gmail.com>
2026-01-23 21:53:35 +0300

    8.1.16 Написание моделей Pydantic

---
+++ pydantic_create_user.py
@@ -0,0 +1,53 @@
+from pydantic import BaseModel, Field, HttpUrl, EmailStr, ValidationError, ConfigDict
+from pydantic.alias_generators import to_camel
+
+class CreateUserRequestSchema(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel, validate_by_alias=True)
+    email: EmailStr  # Используем EmailStr вместо str
+    last_name: str
+    first_name: str
+    middle_name: str
+    phone_number: str
+
+class UserSchema(CreateUserRequestSchema):
+    id: str
+
+class CreateUserResponseSchema(BaseModel):
+    user: UserSchema
+
+create_user_dict = {
+  "email": "user@example.com",
+  "lastName": "string",
+  "firstName": "string",
+  "middleName": "string",
+  "phoneNumber": "string"
+}
+user_dict = {
+  "id": "string",
+  "email": "user@example.com",
+  "lastName": "string",
+  "firstName": "string",
+  "middleName": "string",
+  "phoneNumber": "string"
+}
+create_user_response_dict = {
+  "user": {
+    "id": "string",
+    "email": "user@example.com",
+    "lastName": "string",
+    "firstName": "string",
+    "middleName": "string",
+    "phoneNumber": "string"
+  }
+}
+
+# create_user_model = CreateUserRequestSchema(**create_user_dict)
+# print(f'{create_user_model=}')
+#
+# user_model = UserSchema(**user_dict)
+# print(f'{user_model=}')
+#
+# create_user_response_model = CreateUserResponseSchema(**create_user_response_dict)
+# print(f'{create_user_response_model=}')
+# print(create_user_response_model.model_dump(by_alias=True))
+
