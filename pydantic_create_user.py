800b1eeb5edd270910588f71056faa05142cad23
tatyderb <tatyderb@gmail.com>
2026-01-27 22:38:06 +0300

    8.1.16 Еще немного экспериментов с snack_case и camelCase.

--- pydantic_create_user.py
+++ pydantic_create_user.py
@@ -41,13 +41,19 @@ create_user_response_dict = {
   }
 }
 
-# create_user_model = CreateUserRequestSchema(**create_user_dict)
-# print(f'{create_user_model=}')
-#
-# user_model = UserSchema(**user_dict)
-# print(f'{user_model=}')
-#
-# create_user_response_model = CreateUserResponseSchema(**create_user_response_dict)
-# print(f'{create_user_response_model=}')
-# print(create_user_response_model.model_dump(by_alias=True))
+create_user_model = CreateUserRequestSchema(**create_user_dict)
+print(f'{create_user_model=}')
+
+user_model = UserSchema(**user_dict)
+print(f'{user_model=}')
+
+create_user_response_model = CreateUserResponseSchema(**create_user_response_dict)
+print(f'{create_user_response_model=}')
+print(create_user_response_model.model_dump(by_alias=True))
+
+print('-----------------')
+text='{"user":{"id":"418f4ba6-9a59-4fbc-8a36-4f9886c3c6a5","email":"user.1769283774.6198812@example.com","lastName":"string","firstName":"string","middleName":"string","phoneNumber":"string"}}'
+create_user_response_model = CreateUserResponseSchema.model_validate_json(text)
+print(f'{create_user_response_model=}')
+print(create_user_response_model.model_dump(by_alias=True))
 
