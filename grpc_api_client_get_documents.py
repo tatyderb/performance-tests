c561d93b6bbb2f72458454f938fd7754d8ea3b51
tatyderb <tatyderb@gmail.com>
2026-02-01 00:20:28 +0300

    9.5.15 - gRPC: получение документов по счету

---
+++ grpc_api_client_get_documents.py
@@ -0,0 +1,29 @@
+# Импортируем фабричные функции для создания API-клиентов
+from clients.grpc.gateway.accounts.client import build_accounts_gateway_grpc_client
+from clients.grpc.gateway.documents.client import build_documents_gateway_grpc_client
+from clients.grpc.gateway.users.client import build_users_gateway_grpc_client
+
+# Создаём API-клиенты для работы с сервисами Users, Accounts и Cards
+users_gateway_client = build_users_gateway_grpc_client()
+accounts_gateway_client = build_accounts_gateway_grpc_client()
+documents_gateway_client = build_documents_gateway_grpc_client()
+
+# Шаг 1. Создаём пользователя
+create_user_response = users_gateway_client.create_user()
+print('Create user response:', create_user_response)
+
+# Шаг 2. Открываем кредитный счёт для только что созданного пользователя
+open_credit_card_account_response = accounts_gateway_client.open_credit_card_account(
+    user_id=create_user_response.user.id
+)
+print('Open credit card account response:', open_credit_card_account_response)
+
+account_id = open_credit_card_account_response.account.id
+
+# Шаг 3. Получить документ тарифа
+tariff_document_response = documents_gateway_client.get_tariff_document(account_id=account_id)
+print('Get tariff document response:', tariff_document_response)
+
+# Шаг 4. Получить документ контракта
+contract_document_response = documents_gateway_client.get_contract_document(account_id=account_id)
+print('Get contract document response:', contract_document_response)
