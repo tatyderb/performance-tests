98ce6100b7d236cd5eefabacc505ff24c9c0d685
tatyderb <tatyderb@gmail.com>
2026-02-06 22:03:24 +0300

    10.5.7 locust + grpc + account client.

--- grpc_locust_open_debit_card_account.py
+++ grpc_locust_open_debit_card_account.py
@@ -1,10 +1,11 @@
 from locust import User, between, task
 
+from clients.grpc.gateway.accounts.client import AccountsGatewayGRPCClient, build_accounts_gateway_locust_grpc_client
 from clients.grpc.gateway.users.client import UsersGatewayGRPCClient, build_users_gateway_locust_grpc_client
 from contracts.services.gateway.users.rpc_create_user_pb2 import CreateUserResponse
 
 
-class GetUserScenarioUser(User):
+class OpenDebitCardAccountScenarioUser(User):
     # Атрибут host обязателен для Locust, даже если он не используется напрямую в gRPC.
     host = "localhost"
     # Время ожидания между задачами от 1 до 3 секунд.
@@ -12,6 +13,8 @@ class GetUserScenarioUser(User):
 
     # Аннотации для клиентов и ответов
     users_gateway_client: UsersGatewayGRPCClient
+    accounts_gateway_client: AccountsGatewayGRPCClient
+
     create_user_response: CreateUserResponse
 
     def on_start(self) -> None:
@@ -21,6 +24,7 @@ class GetUserScenarioUser(User):
         """
         # Инициализируем gRPC-клиент, пригодный для Locust, с интерцептором метрик.
         self.users_gateway_client = build_users_gateway_locust_grpc_client(self.environment)
+        self.accounts_gateway_client = build_accounts_gateway_locust_grpc_client(self.environment)
 
         # Создаём пользователя один раз в начале сессии.
         self.create_user_response = self.users_gateway_client.create_user()
@@ -28,9 +32,10 @@ class GetUserScenarioUser(User):
         # print(f'USER ID = {self.create_user_response.user.id}')
 
     @task
-    def get_user(self):
+    def open_debit_card_account(self):
         """
         Основная задача виртуального пользователя — получение данных пользователя.
         Метод будет многократно вызываться Locust-агентами.
         """
-        self.users_gateway_client.get_user(self.create_user_response.user.id)
+        resp = self.accounts_gateway_client.open_debit_card_account(self.create_user_response.user.id)
+        # print(f'ACCOUNT DATA = {resp.account}')
