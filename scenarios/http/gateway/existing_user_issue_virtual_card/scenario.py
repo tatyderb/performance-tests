10a432fd6a1968448c1c8e832769357aec02d6c9
tatyderb <tatyderb@gmail.com>
2026-02-14 00:47:01 +0300

    12.3.10 Выпуск виртуальной карты существующим пользователем

---
+++ scenarios/http/gateway/existing_user_issue_virtual_card/scenario.py
@@ -0,0 +1,46 @@
+from locust import task, events
+from locust.env import Environment
+
+from clients.http.gateway.locust import GatewayHTTPTaskSet
+from seeds.scenarios.existing_user_issue_virtual_card import ExistingUserIssueVirtualCardSeedsScenario
+from seeds.schema.result import SeedUserResult
+from tools.locust.user import LocustBaseUser
+
+
+# Хук инициализации — вызывается перед началом запуска нагрузки
+@events.init.add_listener
+def init(environment: Environment, **kwargs):
+    # Выполняем сидинг
+    seeds_scenario = ExistingUserIssueVirtualCardSeedsScenario()
+    seeds_scenario.build()  # создаём пользователей, счета, карты и операции
+
+    # Загружаем результат сидинга (из файла JSON)
+    environment.seeds = seeds_scenario.load()
+
+
+# TaskSet — сценарий пользователя. Каждый виртуальный пользователь выполняет эти задачи
+class IssueVirtualCardTaskSet(GatewayHTTPTaskSet):
+    seed_user: SeedUserResult  # Типизированная ссылка на данные из сидинга
+
+    def on_start(self) -> None:
+        super().on_start()
+        # Получаем случайного пользователя из подготовленного списка
+        self.seed_user = self.user.environment.seeds.get_random_user()
+
+    @task(3)
+    def get_accounts(self):
+        # Получаем список счетов пользователя
+        self.accounts_gateway_client.get_accounts(user_id=self.seed_user.user_id)
+
+    @task(1)
+    def issue_virtual_card(self):
+        # выпускаем виртуальную карту
+        self.cards_gateway_client.issue_virtual_card(
+            user_id=self.seed_user.user_id,
+            account_id=self.seed_user.credit_card_accounts[0].account_id
+        )
+
+
+# Пользовательский класс, который будет запускать наш TaskSet
+class IssueVirtualCardScenarioUser(LocustBaseUser):
+    tasks = [IssueVirtualCardTaskSet]
