f2f31e108764324c501da00a171fbc5aff6277e8
tatyderb <tatyderb@gmail.com>
2026-02-21 22:29:02 +0300

    15.1.16 Вынесение конфигурации для http + grpc client.

--- clients/http/gateway/client.py
+++ clients/http/gateway/client.py
@@ -7,6 +7,7 @@ from clients.http.event_hooks.locust_event_hook import (
     locust_request_event_hook,  # Хук для отслеживания начала запроса
     locust_response_event_hook  # Хук для сбора метрик по завершении запроса
 )
+from config import settings
 
 
 def build_gateway_http_client() -> Client:
@@ -15,7 +16,10 @@ def build_gateway_http_client() -> Client:
 
     :return: Готовый к использованию объект httpx.Client.
     """
-    return Client(timeout=100, base_url="http://localhost:8003")
+    return Client(
+        timeout=settings.gateway_http_client.timeout,
+        base_url=settings.gateway_http_client.client_url,
+    )
 
 
 def build_gateway_locust_http_client(environment: Environment) -> Client:
@@ -38,8 +42,8 @@ def build_gateway_locust_http_client(environment: Environment) -> Client:
     logging.getLogger("httpx").setLevel(logging.WARNING)
 
     return Client(
-        timeout=100,
-        base_url="http://localhost:8003",
+        timeout=settings.gateway_http_client.timeout,
+        base_url=settings.gateway_http_client.client_url,
         event_hooks={
             "request": [locust_request_event_hook],  # Отмечаем время начала запроса
             "response": [locust_response_event_hook(environment)]  # Собираем метрики и передаём их в Locust
