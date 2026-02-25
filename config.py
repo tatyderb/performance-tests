f0f529678c513100317a6414bee3c2caeab1754c
tatyderb <tatyderb@gmail.com>
2026-02-26 00:52:41 +0300

    17.4.2 Загрузка в  Load Testing Hub результатов сценариев existing user ...

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import locust.stats  # Модуль Locust, отвечающий за сбор и хранение статистики
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 # Импортируем вложенные модели
@@ -5,6 +6,21 @@ from tools.config.grpc import GRPCClientConfig
 from tools.config.http import HTTPClientConfig
 from tools.config.locust import LocustUserConfig
 
+
+# Настройка списка процентилей, которые будут попадать в отчёты Locust
+locust.stats.PERCENTILES_TO_REPORT = [0.50, 0.60, 0.70, 0.80, 0.90, 0.95, 0.99, 1.0]
+
+# Интервал (в секундах) между записями агрегированной статистики в CSV
+locust.stats.CSV_STATS_INTERVAL_SEC = 5
+
+# Интервал (в секундах) между записями "исторической" статистики (динамика значений)
+locust.stats.HISTORY_STATS_INTERVAL_SEC = 5
+
+# Интервал (в секундах) между обновлением статистики в консоли Locust
+locust.stats.CONSOLE_STATS_INTERVAL_SEC = 5
+
+# Интервал (в секундах) между принудительной записью CSV на диск
+locust.stats.CSV_STATS_FLUSH_INTERVAL_SEC = 5
 
 class Settings(BaseSettings):
     # Конфигурация загрузки — откуда брать переменные
